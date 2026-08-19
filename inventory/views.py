fb0fe8a1fc96c0287e4ced38c7bbb1a38900c870
Shuxrat <shuxratmuxamatkulov@gmail.com>
2026-08-19 12:07:30 +0500

    Allow public access to products and categories

--- inventory/views.py
+++ inventory/views.py
@@ -3,17 +3,20 @@ from rest_framework.permissions import AllowAny
 from .models import Category, Product, Transaction
 from .serializers import CategorySerializer, ProductSerializer, TransactionSerializer
 
+
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [AllowAny]
-    authentication_classes = []  # JWT tekshiruvini ushbu view uchun to'liq o'chirish
+    authentication_classes = []  # Autentifikatsiya cheklovini to'liq o'chirish
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [AllowAny]
-    authentication_classes = []  # JWT tekshiruvini ushbu view uchun to'liq o'chirish
+    authentication_classes = []  # Autentifikatsiya cheklovini to'liq o'chirish
+
 
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
